204abd937dc84e23c5f64cbd66d8f7f3c8c990e0
Muskan83712 <108280794+Muskan83712@users.noreply.github.com>
2024-03-10 15:41:21 +0530

    q5.c

--- q5.c
+++ q5.c
@@ -22,5 +22,9 @@ printf("n2 is greater\n");
 else
 printf("n3 is greater\n");
 
+// using conditional operator
+ 
+((n1>n2) && (n1>n3)) ? printf("n1 is greater") : ((n2>n1) && (n2>n3) ? printf("n2 is greater") : printf("n3 is grater"));
+ 
 return 0;
 }
