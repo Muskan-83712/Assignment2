7a8eea0a8e0a5ec1c2d4928711dcf7402855b85d
MUSKAN <muskanprajapati0609@gmail.com>
2024-03-10 15:27:17 +0530

    question5

---
+++ q5.c
@@ -0,0 +1,26 @@
+/*Write a program to find maximum of three numbers using
+a. If – else
+b. conditional operator.*/
+
+#include<stdio.h>
+int main()
+{
+ int n1,n2,n3;
+ printf("Entter three numbers :\n");
+ scanf("%d%d%d",&n1,&n2,&n3);
+ 
+ // using if else
+
+if ((n1>n2) && (n1>n3))
+{
+printf("n1 is greater\n");
+
+}
+else if ((n2>n1)&&(n2>n3))
+printf("n2 is greater\n");
+
+else
+printf("n3 is greater\n");
+
+return 0;
+}
